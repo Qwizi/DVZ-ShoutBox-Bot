40f8a2e5961a73bed983ce11edd5322b1daa52a4
Adrian Ciołek <qwizi@sharkservers.pl>
2018-04-14 21:16:34 +0200

    1.4 version

---
+++ root/inc/plugins/dvz_shoutbox_bot.php
@@ -0,0 +1,155 @@
+<?php
+if(!defined('IN_MYBB'))
+{
+   die('Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.');
+}
+
+$plugins->add_hook('member_do_register_end', 'dvz_shoutbox_bot_action_register');
+$plugins->add_hook('datahandler_post_insert_thread_end', 'dvz_shoutbox_bot_action_thread');
+$plugins->add_hook('datahandler_post_insert_post_end', 'dvz_shoutbox_bot_action_post');
+$plugins->add_hook('dvz_shoutbox_shout_commit', 'dvz_shoutbox_bot_action');
+$plugins->add_hook('admin_user_menu', 'dvz_shoutbox_bot_admin_user_menu');
+$plugins->add_hook('admin_user_action_handler', 'dvz_shoutbox_bot_user_action_handler');
+
+function dvz_shoutbox_bot_info()
+{
+    global $lang, $db;
+    $lang->load('dvz_shoutbox_bot');
+
+   return [
+      'name'            => $db->escape_string($lang->bot_title),
+      'description'     => $db->escape_string($lang->bot_desc),
+      'website'         => 'http://sharkservers.eu',
+      'author'         => 'Adrian \'Qwizi\' Ciołek',
+      'authorsite'    => 'http://sharkservers.eu',
+      'version'         => '1.4',
+      'codename'     => '',
+      'compatibility'   => '18*',
+   ];
+}
+
+function dvzBot_init()
+{
+   require_once MYBB_ROOT.'inc/plugins/dvz_shoutbox_bot_class.php';
+   return dvz_shoutbox_bot::init();
+}
+
+function dvz_shoutbox_bot_is_installed()
+{
+   $dvzBot = dvzBot_init();
+   return $dvzBot->is_installed();
+}
+
+function dvz_shoutbox_bot_install()
+{
+   $dvzBot = dvzBot_init();
+   $dvzBot->install();
+}
+
+function dvz_shoutbox_bot_uninstall()
+{
+   $dvzBot = dvzBot_init();
+   $dvzBot->uninstall();
+}
+
+function dvz_shoutbox_bot_admin_user_menu(&$sub_menu)
+{
+   global $lang;
+   $lang->load('dvz_shoutbox_bot');
+   $sub_menu[] = ['id' => 'dvz_shoutbox_bot', 'title' => 'DVZ ShoutBox Bot', 'link' => 'index.php?module=user-dvz_shoutbox_bot'];
+}
+
+function dvz_shoutbox_bot_user_action_handler(&$actions)
+{
+   $actions['dvz_shoutbox_bot'] = ['active' => 'dvz_shoutbox_bot', 'file' => 'dvz_shoutbox_bot.php'];
+}
+
+function dvz_shoutbox_bot_action_register()
+{
+   global $db, $mybb;
+   if($mybb->settings['dvz_shoutbox_bot_on'] && $mybb->settings['dvz_shoutbox_bot_register_on'])
+   {
+      $row = $db->fetch_array($db->simple_select("users", "username", "", ['order_by' => 'regdate DESC', 'limit' => 1]));
+      $bot['message'] = $mybb->settings['dvz_shoutbox_bot_register_message'];
+      if($mybb->settings['dvz_shoutbox_bot_link_on'])
+      {
+         $bot['message'] = str_replace('{username}', "@\"".$row['username']."\"", $bot['message']);
+      }else{
+         $bot['message'] = str_replace('{username}', $row['username'], $bot['message']);
+      }
+      $dvzBot = dvzBot_init();
+      $dvzBot->shout($bot['message']);
+   }
+}
+
+function dvz_shoutbox_bot_action_thread()
+{
+   global $db, $mybb, $new_thread;
+   if($mybb->settings['dvz_shoutbox_bot_on'] && $mybb->settings['dvz_shoutbox_bot_thread_on'])
+   {
+      $row = $db->fetch_array($db->simple_select("threads", "tid, fid, username, subject", "", ['order_by' => 'dateline DESC', 'limit' => 1]));
+
+      $forum = get_forum($row['fid']);
+      $bot['message'] = $mybb->settings['dvz_shoutbox_bot_thread_message'];
+      $link = '[url='.$mybb->settings['bburl'].'/'.get_thread_link($row['tid']).']'.$db->escape_string($row['subject']).'[/url]';
+      $link2 = '[url='.$mybb->settings['bburl'].'/'.get_forum_link($row['fid']).']'.$db->escape_string($forum['name']).'[/url]';
+      $bot['message'] = str_replace('{subject}', $link, $bot['message']);
+      $bot['message'] = str_replace('{forum}', $link2, $bot['message']);
+      if($mybb->settings['dvz_shoutbox_bot_link_on'])
+      {
+         $bot['message'] = str_replace('{username}', "@\"".$thread['username']."\"", $bot['message']);
+      }else{
+         $bot['message'] = str_replace('{username}', $row['username'], $bot['message']);
+      }
+      if(!$new_thread['savedraft'])
+      {
+         $ignore = explode(',', $mybb->settings['dvz_shoutbox_bot_ignore']);
+         if(!in_array($row['fid'], $ignore))
+         {
+            $dvzBot = dvzBot_init();
+            $dvzBot->shout($bot['message']);
+         }
+      }
+   }
+}
+
+function dvz_shoutbox_bot_action_post()
+{
+   global $db, $mybb;
+   if($mybb->settings['dvz_shoutbox_bot_on'] && $mybb->settings['dvz_shoutbox_bot_post_on'])
+   {
+      $row = $db->fetch_array($db->simple_select("posts", "pid, tid, fid, username, subject", "", ['order_by' => 'dateline DESC', 'limit' => 1]));
+
+      $link = '[url='.$mybb->settings['bburl'].'/'.get_post_link($row['pid'], $row['tid']).'#pid'.$row['pid'].']'.$row['subject'].'[/url]';
+      $bot['message'] = $mybb->settings['dvz_shoutbox_bot_post_message'];
+      $bot['message'] = str_replace('{subject}', $link, $bot['message']);
+      if($mybb->settings['dvz_shoutbox_bot_link_on'])
+      {
+         $bot['message'] = str_replace('{username}', "@\"".$row['username']."\"", $bot['message']);
+      }else{
+         $bot['message'] = str_replace('{username}', $row['username'], $bot['message']);
+      }
+      $ignore = explode(',', $mybb->settings['dvz_shoutbox_bot_ignore']);
+      if(!in_array($row['fid'], $ignore))
+      {
+         $dvzBot = dvzBot_init();
+         $dvzBot->shout($bot['message']);
+      }
+   }
+}
+
+function dvz_shoutbox_bot_action(&$data)
+{
+   global $db, $mybb;
+   if($mybb->settings['dvz_shoutbox_bot_on'] && $mybb->settings['dvz_shoutbox_bot_action_on'])
+   {
+      $data['text'] = $db->escape_string($data['text']);
+      $row = $db->fetch_array($db->simple_select("dvz_shoutbox_bot", "string, answer", "string='{$data['text']}'"));
+      if($data['text'] === $row['string'])
+      {
+         $dvzBot = dvzBot_init();
+         $dvzBot->shout($row['answer']);
+      }
+   }
+}
+?>
---
+++ root/inc/plugins/dvz_shoutbox_bot_class.php
@@ -0,0 +1,217 @@
+<?php
+define("IN_MYBB", 1);
+
+class dvz_shoutbox_bot
+{
+   public static $init = null;
+   public $mybb;
+   public $db;
+   public $lang;
+   public $bot_id;
+
+   private function __construct()
+   {
+      global $mybb, $db, $lang;
+      $this->mybb =& $mybb;
+      $this->db =& $db;
+      $this->lang =& $lang;
+      if($this->is_installed())
+      {
+         $this->bot_id = $this->mybb->settings['dvz_shoutbox_bot_id'];
+      }
+   }
+
+   public static function init()
+   {
+      if(!isset(self::$init))
+      {
+         self::$init = new dvz_shoutbox_bot();
+      }
+      return self::$init;
+   }
+
+   public function shout($text)
+   {
+      $data = [
+         'uid' => $this->bot_id,
+         'text' => $text,
+         'date' => TIME_NOW
+      ];
+      foreach ($data as $key => &$value)
+      {
+          if (!in_array($key, array_keys($this->mybb->binary_fields['dvz_shoutbox'])))
+          {
+             $value = $this->db->escape_string($value);
+         }
+      }
+     return $this->db->insert_query('dvz_shoutbox', $data);
+   }
+
+   public function addPanelMessage($string, $answer)
+   {
+      $data['string'] = $this->db->escape_string($string);
+      $data['answer'] = $this->db->escape_string($answer);
+      return $this->db->insert_query('dvz_shoutbox_bot', $data);
+   }
+
+   public function is_installed()
+   {
+      return $this->mybb->settings['dvz_shoutbox_bot_on'] !== null;
+   }
+
+   public function install()
+   {
+      $this->uninstall();
+      $this->lang->load('dvz_shoutbox_bot');
+
+      $max_disporder = $this->db->fetch_field($this->db->simple_select('settinggroups', 'MAX(disporder) AS max_disporder'), 'max_disporder');
+
+      $group = [
+         'gid'             => 'NULL',
+         'name'         => 'dvz_shoutbox_bot',
+         'title'            => $this->db->escape_string($this->lang->bot_title),
+         'description'  => $this->db->escape_string($this->lang->bot_setting_desc),
+         'disporder'     => $max_disporder + 1,
+         'isdefault'       => '0',
+      ];
+      $this->db->insert_query('settinggroups', $group);
+      $gid = $this->db->insert_id();
+
+      $settings  = [
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_on',
+            'title'             => $this->db->escape_string($this->lang->bot_onoff_title),
+            'description'  => $this->db->escape_string($this->lang->bot_onoff_desc),
+            'optionscode'   => 'onoff',
+            'value'           => '1',
+            'disporder'     => '1',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_link_on',
+            'title'             => $this->db->escape_string($this->lang->bot_link_title),
+            'description'  => $this->db->escape_string($this->lang->bot_link_desc),
+            'optionscode'   => 'yesno',
+            'value'           => 'yes',
+            'disporder'     => '2',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_action_on',
+            'title'             => $this->db->escape_string($this->lang->bot_action_title),
+            'description'  => $this->db->escape_string($this->lang->bot_action_desc),
+            'optionscode'   => 'yesno',
+            'value'           => 'yes',
+            'disporder'     => '3',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_id',
+            'title'             => $this->db->escape_string($this->lang->bot_id_title),
+            'description'  => $this->db->escape_string($this->lang->bot_id_desc),
+            'optionscode'   => 'numeric',
+            'value'           => '1',
+            'disporder'     => '4',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_register_on',
+            'title'             => $this->db->escape_string($this->lang->bot_register_title),
+            'description'  => $this->db->escape_string($this->lang->bot_register_desc),
+            'optionscode'   => 'yesno',
+            'value'           => 'yes',
+            'disporder'     => '5',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_register_message',
+            'title'             => $this->db->escape_string($this->lang->bot_register_message_title),
+            'description'  => $this->db->escape_string($this->lang->bot_register_message_desc),
+            'optionscode'   => 'textarea',
+            'value'           => $this->lang->bot_register_message_example,
+            'disporder'     => '6',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_thread_on',
+            'title'             => $this->db->escape_string($this->lang->bot_thread_title),
+            'description'  => $this->db->escape_string($this->lang->bot_thread_desc),
+            'optionscode'   => 'yesno',
+            'value'           => 'yes',
+            'disporder'     => '7',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_ignore',
+            'title'             => $this->db->escape_string($this->lang->bot_ignore_title),
+            'description'  => $this->db->escape_string($this->lang->bot_ignore_desc),
+            'optionscode'   => 'forumselect',
+            'value'           => '',
+            'disporder'     => '8',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_thread_message',
+            'title'             => $this->db->escape_string($this->lang->bot_thread_message_title),
+            'description'  => $this->db->escape_string($this->lang->bot_thread_message_desc),
+            'optionscode'   => 'textarea',
+            'value'           =>$this->lang->bot_thread_message_example,
+            'disporder'     => '9',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_post_on',
+            'title'             => $this->db->escape_string($this->lang->bot_post_title),
+            'description'  => $this->db->escape_string($this->lang->bot_post_desc),
+            'optionscode'   => 'yesno',
+            'value'           => 'yes',
+            'disporder'     => '10',
+            'gid'              => intval($gid),
+         ],
+         [
+            'sid'             => 'NULL',
+            'name'         => 'dvz_shoutbox_bot_post_message',
+            'title'             => $this->db->escape_string($this->lang->bot_post_message_title),
+            'description'  => $this->db->escape_string($this->lang->bot_post_message_desc),
+            'optionscode'   => 'textarea',
+            'value'           => $this->lang->bot_post_message_example,
+            'disporder'     => '11',
+            'gid'              => intval($gid),
+         ],
+      ];
+      $this->db->insert_query_multiple("settings", $settings);
+      rebuild_settings();
+
+      if(!$this->db->table_exists('dvz_shoutbox_bot'))
+      {
+         $this->db->write_query('CREATE TABLE `'.TABLE_PREFIX.'dvz_shoutbox_bot` ( `id` INT NOT NULL AUTO_INCREMENT , `string` TEXT NOT NULL , `answer` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci'.$this->db->build_create_table_collation().';');
+      }
+
+      if($this->db->table_exists('dvz_shoutbox_bot'))
+      {
+         $this->addPanelMessage('Cześć', 'Witaj kolego');
+      }
+   }
+
+   public function uninstall()
+   {
+      $this->db->delete_query('settinggroups', 'name=\'dvz_shoutbox_bot\'');
+      $this->db->delete_query('settings', 'name LIKE \'dvz_shoutbox_bot%\'');
+      if($this->db->table_exists('dvz_shoutbox_bot'))
+      {
+         $this->db->drop_table('dvz_shoutbox_bot');
+      }
+      rebuild_settings();
+   }
+}
+ ?>
