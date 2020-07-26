a324cc29cb12ec27f524db139d57ced6b8efa625
Adrian Ciołek <qwizi95@protonmail.com>
2020-07-26 11:20:04 +0200

    Removed task

--- inc/tasks/dvz_shoutbox_bot.php
+++
@@ -1,9 +0,0 @@
-<?php
-function task_dvz_shoutbox_bot($task)
-{
-	global $mybb, $db;
-    
-    $db->delete_query('dvz_shoutbox', "text IS NULL");
-
-	add_task_log($task, "Usunieto niepotrzebne wpisy");
-}
